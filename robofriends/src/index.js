cb63f686d6824123828442417495b77eb5359e91
Westklee <westklee@gmail.com>
2018-07-04 14:05:10 -0500

    robot search app

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import App from './containers/App';
+import registerServiceWorker from './components/registerServiceWorker';
 import 'tachyons'; 
 
 
-ReactDOM.render(
+ReactDOM.render( 
 	<App />, document.getElementById('root'));
 registerServiceWorker();
